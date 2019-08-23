8d64c3f464526f5701f55883f66519d84088a69a
lee <leesherlock13@gmail.com>
2019-08-23 21:57:14 +1000

    Raghul, I make a logout button, easy to go back main screen for testing.

--- app/src/main/java/com/example/industrialexperiencee15/Dashboard.java
+++ app/src/main/java/com/example/industrialexperiencee15/Dashboard.java
@@ -6,13 +6,30 @@ import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 
+import com.google.firebase.auth.FirebaseAuth;
+
 public class Dashboard extends AppCompatActivity {
+    FirebaseAuth mFirebaseAuth;
+    private FirebaseAuth.AuthStateListener mAuthStateListener;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dashboard);
 
         final Button Insert = (Button) findViewById(R.id.btnTile1);
+        final Button logout = (Button) findViewById(R.id.logout);
+
+
+
+        logout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                FirebaseAuth.getInstance().signOut();
+                Intent out = new Intent(Dashboard.this, MainActivity.class);
+                Dashboard.this.startActivity(out);
+            }
+        });
 
 
         Insert.setOnClickListener(new View.OnClickListener() {
@@ -22,6 +39,7 @@ public class Dashboard extends AppCompatActivity {
                 Dashboard.this.startActivity(insert);
             }
         });
+
 
     }
 }
