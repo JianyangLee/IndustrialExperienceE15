a34a932ff5e1fd1f8e90fa9d962c6ac02f82a7e7
RaghulRavichandranR <rrav0006@student.monash.edu>
2019-09-14 15:59:39 +1000

    Facilites Search  Working
    The Facilites screen working Now

--- app/src/main/java/com/example/industrialexperiencee15/Facilities.java
+++ app/src/main/java/com/example/industrialexperiencee15/Facilities.java
@@ -1,12 +1,192 @@
 package com.example.industrialexperiencee15;
 
 
+import android.content.Context;
+import android.content.Intent;
+import android.os.AsyncTask;
+import android.os.Build;
 import android.os.Bundle;
+import android.os.VibrationEffect;
+import android.os.Vibrator;
 import android.support.v7.app.AppCompatActivity;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.util.Log;
+import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.view.inputmethod.InputMethodManager;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ListView;
 
+import com.google.firebase.auth.FirebaseAuth;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Calendar;
+
 public class Facilities extends AppCompatActivity {
+    EditText facilitiesName;
+    ArrayList<String> listItems;
+    ArrayAdapter<String> adapter;
+    ListView listView;
+    String[] facilitiesList;
+    String userID;
+    JSONArray jsonArr;
 
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_facilities);
 
+        listView = (ListView) findViewById(R.id.facilitiesListView);
 
+        // userID = FirebaseAuth.getInstance().getCurrentUser().getUid();
+
+        Facilities.GetAllFacilitiesAsync AllFacilitiesAsync = new Facilities.GetAllFacilitiesAsync();
+        AllFacilitiesAsync.execute();
+
+        facilitiesName = (EditText) findViewById(R.id.facilityName);
+        final Button backToDash = (Button) findViewById(R.id.btnBacktoDashboard);
+        final Button btnSearch = (Button) findViewById(R.id.startNavigation);
+
+        facilitiesName.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                if (s.toString().equals("")) {
+                    initialList();
+                } else {
+                    searchItem(s.toString());
+                }
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+
+            }
+        });
+
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                hideKeyboard();
+                Object item = parent.getItemAtPosition(position);
+                String value = item.toString();
+                facilitiesName.setText(value);
+            }
+        });
+
+        backToDash.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent backToDash = new Intent(Facilities.this, Dashboard.class);
+                Facilities.this.startActivity(backToDash);
+            }
+        });
+
+        btnSearch.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (facilitiesName.getText().toString().equals("")) {
+                    Animation shake = AnimationUtils.loadAnimation(Facilities.this, R.anim.shake);
+                    facilitiesName.startAnimation(shake);
+                    facilitiesName.setError("Please choose an item");
+                    vibrateField();
+                    return;
+                }
+            }
+        });
     }
+
+
+    private void initialList() {
+        listItems = new ArrayList<>(Arrays.asList(facilitiesList));
+        adapter = new ArrayAdapter<String>(this, R.layout.list_item, R.id.textView, listItems);
+        listView.setAdapter(adapter);
+    }
+
+    private void searchItem(String textSearch) {
+        initialList();
+        for (String exercise : facilitiesList) {
+            if (!exercise.toLowerCase().contains(textSearch.toLowerCase())) {
+                listItems.remove(exercise);
+            }
+        }
+        adapter.notifyDataSetChanged();
+    }
+
+    private void hideKeyboard() {
+        View view = this.getCurrentFocus();
+        if (view != null) {
+            view.clearFocus();
+            InputMethodManager inputManager = (InputMethodManager) this.getSystemService(Context.INPUT_METHOD_SERVICE);
+            inputManager.hideSoftInputFromWindow(view.getWindowToken(), InputMethodManager.HIDE_NOT_ALWAYS);
+        }
+    }
+
+    public static boolean isInteger(String s) {
+        try {
+            Integer.parseInt(s);
+        } catch (NumberFormatException e) {
+            return false;
+        } catch (NullPointerException e) {
+            return false;
+        }
+        // only got here if we didn't return false
+        return true;
+    }
+
+    private void vibrateField() {
+        Vibrator v = (Vibrator) getSystemService(Context.VIBRATOR_SERVICE);
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            v.vibrate(VibrationEffect.createOneShot(100, VibrationEffect.DEFAULT_AMPLITUDE));
+        } else {
+            //deprecated in API 26
+            v.vibrate(100);
+        }
+    }
+
+    private class GetAllFacilitiesAsync extends AsyncTask<String, Void, String> {
+        @Override
+        protected String doInBackground(String... params) {
+            try {
+                String returnValue = RestService.getAllExercise();
+                JSONObject jsnobject = new JSONObject(returnValue);
+                jsonArr = jsnobject.getJSONArray("data");
+                facilitiesList = new String[jsonArr.length()];
+
+                for (int i = 0; i < jsonArr.length(); i++) {
+                    try {
+                        JSONObject obj = jsonArr.getJSONObject(i);
+                        String name = obj.getString("EXERCISE_NAME");
+                        facilitiesList[i] = name;
+                    } catch (Exception e) {
+                        Log.e("test", "get test");
+                    }
+                }
+            } catch (Exception e) {
+
+            }
+
+            return "";
+        }
+
+        @Override
+        protected void onPostExecute(String response) {
+            initialList();
+        }
+    }
+
+}
--- app/src/main/java/com/example/industrialexperiencee15/Maps_Facilities.java
+++ app/src/main/java/com/example/industrialexperiencee15/Maps_Facilities.java
@@ -23,60 +23,96 @@ import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
 
 
-    public class Maps_Facilities extends FragmentActivity implements OnMapReadyCallback, GoogleMap.OnMapLongClickListener {
-        private Location currentLocation;
-        private FusedLocationProviderClient fusedLocationProviderClient;
-        private static final int LOCATION_REQUEST_CODE = 101;
-        private GoogleMap googleMap;
-        TextView tvLocInfo;
+public class Maps_Facilities extends FragmentActivity implements OnMapReadyCallback, GoogleMap.OnMapLongClickListener {
+    private Location currentLocation;
+    private FusedLocationProviderClient fusedLocationProviderClient;
+    private static final int LOCATION_REQUEST_CODE = 101;
+    private GoogleMap mMap;
+    TextView tvLocInfo;
 
-        @Override
-        protected void onCreate(Bundle savedInstanceState) {
-            super.onCreate(savedInstanceState);
-            setContentView(R.layout.activity_facilities);
-            fusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(this);
-            if (ActivityCompat.checkSelfPermission(Maps_Facilities.this, android.Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(Maps_Facilities.this, android.Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
-                ActivityCompat.requestPermissions(this, new String[]{android.Manifest.permission.ACCESS_FINE_LOCATION}, LOCATION_REQUEST_CODE);
-                return;
-            }
-            fetchLastLocation();
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_maps__facilities);
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.Facilities_Map);
+        mapFragment.getMapAsync(this);
 
+        fusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(this);
+        if (ActivityCompat.checkSelfPermission(Maps_Facilities.this, android.Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(Maps_Facilities.this, android.Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this, new String[]{android.Manifest.permission.ACCESS_FINE_LOCATION}, LOCATION_REQUEST_CODE);
+            return;
         }
+        fetchLastLocation();
 
-        private void fetchLastLocation() {
+    }
+
+    private void fetchLastLocation() {
 
 
-            if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
-                // TODO: Consider calling
-                //    ActivityCompat#requestPermissions
-                // here to request the missing permissions, and then overriding
-                //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
-                //                                          int[] grantResults)
-                // to handle the case where the user grants the permission. See the documentation
-                // for ActivityCompat#requestPermissions for more details.
-                return;
-            }
-            Task<Location> task = fusedLocationProviderClient.getLastLocation();
-            task.addOnSuccessListener(new OnSuccessListener<Location>() {
-                @Override
-                public void onSuccess(Location location) {
-                    if (location != null) {
-                        currentLocation = location;
-                        Toast.makeText(Maps_Facilities.this, currentLocation.getLatitude() + " " + currentLocation.getLongitude(), Toast.LENGTH_SHORT).show();
-                        SupportMapFragment supportMapFragment = (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.Facilities_Map);
-                        supportMapFragment.getMapAsync(Maps_Facilities.this);
-                    } else {
-                        Toast.makeText(Maps_Facilities.this, "No Location recorded", Toast.LENGTH_SHORT).show();
-                    }
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+            return;
+        }
+        Task<Location> task = fusedLocationProviderClient.getLastLocation();
+        task.addOnSuccessListener(new OnSuccessListener<Location>() {
+            @Override
+            public void onSuccess(Location location) {
+                if (location != null) {
+                    currentLocation = location;
+                    Toast.makeText(Maps_Facilities.this, currentLocation.getLatitude() + " " + currentLocation.getLongitude(), Toast.LENGTH_SHORT).show();
+                    SupportMapFragment supportMapFragment = (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.Facilities_Map);
+                    supportMapFragment.getMapAsync(Maps_Facilities.this);
+                } else {
+                    Toast.makeText(Maps_Facilities.this, "No Location recorded", Toast.LENGTH_SHORT).show();
                 }
-            });
+            }
+        });
+    }
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+
+        mMap = googleMap;
+        mMap.getUiSettings().setZoomControlsEnabled(true);
+        mMap.getUiSettings().setCompassEnabled(true);
+
+        mMap.getUiSettings().setRotateGesturesEnabled(true);
+        mMap.getUiSettings().setScrollGesturesEnabled(true);
+        mMap.getUiSettings().setTiltGesturesEnabled(true);
+        mMap.getUiSettings().setZoomGesturesEnabled(true);
+        mMap.getUiSettings().setAllGesturesEnabled(true);
+
+
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+            return;
         }
+        mMap.setMyLocationEnabled(true);
 
-        @Override
-        public void onMapReady(GoogleMap googleMap) {
+            mMap.getUiSettings().setMyLocationButtonEnabled(true);
+            //googleMap.setTrafficEnabled(true);
+
+            //googleMap.setOnMapLongClickListener();
+
+            //googleMap.setInfoWindowAdapter(new MyInfoWindowAdapter());
 
 
-            LatLng latLng = new LatLng(currentLocation.getLatitude(), currentLocation.getLongitude());
+
+           // LatLng latLng = new LatLng(currentLocation.getLatitude(), currentLocation.getLongitude());
+
+        LatLng latLng = new LatLng(37.8136, 144.9631);
             //MarkerOptions are used to create a new Marker.You can specify location, title etc with MarkerOptions
             MarkerOptions markerOptions = new MarkerOptions().position(latLng).title("You are Here");
             googleMap.animateCamera(CameraUpdateFactory.newLatLng(latLng));
@@ -93,26 +129,12 @@ import com.google.android.gms.tasks.Task;
             //googleMap.setMyLocationEnabled(true);
             //Location currentLocation = LocationServices.FusedLocationApi.getLastLocation(googleApiClient);
 
-            //googleMap.setMapType(GoogleMap.MAP_TYPE_HYBRID);
+            googleMap.setMapType(GoogleMap.MAP_TYPE_HYBRID);
             //googleMap.setMapType(GoogleMap.MAP_TYPE_NORMAL);
             //googleMap.setMapType(GoogleMap.MAP_TYPE_SATELLITE);
-            googleMap.setMapType(GoogleMap.MAP_TYPE_TERRAIN);
+            //googleMap.setMapType(GoogleMap.MAP_TYPE_TERRAIN);
 
-            googleMap.getUiSettings().setZoomControlsEnabled(true);
-            googleMap.getUiSettings().setCompassEnabled(true);
-            googleMap.getUiSettings().setMyLocationButtonEnabled(true);
-            googleMap.getUiSettings().setRotateGesturesEnabled(true);
-            googleMap.getUiSettings().setScrollGesturesEnabled(true);
-            googleMap.getUiSettings().setTiltGesturesEnabled(true);
-            googleMap.getUiSettings().setZoomGesturesEnabled(true);
-            googleMap.getUiSettings().setAllGesturesEnabled(true);
-            //googleMap.setTrafficEnabled(true);
 
-            //googleMap.setOnMapLongClickListener();
-
-            //googleMap.setInfoWindowAdapter(new MyInfoWindowAdapter());
-
-            //mMap.setMyLocationEnabled(true);
         }
 
         @Override
@@ -120,7 +142,7 @@ import com.google.android.gms.tasks.Task;
 
             tvLocInfo.setText("New marker added@" + point.toString());
 
-            Marker newMarker = googleMap.addMarker(new MarkerOptions()
+            Marker newMarker = mMap.addMarker(new MarkerOptions()
                     .position(point)
                     .snippet(point.toString()));
 
