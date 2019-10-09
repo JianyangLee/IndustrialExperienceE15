dc5686d9fe42c37d0abd3a9763b0384f97b03b6c
lee <leesherlock13@gmail.com>
2019-10-09 22:00:32 +1100

    09/OCT (Lee) copying the ID is done!

--- app/src/main/java/com/example/industrialexperiencee15/ID.java
+++ app/src/main/java/com/example/industrialexperiencee15/ID.java
@@ -1,8 +1,12 @@
 package com.example.industrialexperiencee15;
 
+import android.content.Context;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.TextView;
+import android.text.ClipboardManager;
+import android.widget.Toast;
 
 import com.google.firebase.auth.FirebaseAuth;
 
@@ -20,5 +24,14 @@ public class ID extends AppCompatActivity {
         ID = (TextView) findViewById(R.id.idCopy);
 
         ID.setText(userID);
+
+        ID.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                ClipboardManager cm = (ClipboardManager)ID.this.getSystemService(Context.CLIPBOARD_SERVICE);
+                cm.setText(ID.getText());
+                Toast.makeText(ID.this, "Copied to clipboard", Toast.LENGTH_LONG).show();
+            }
+        });
     }
 }
